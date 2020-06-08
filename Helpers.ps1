e59b76d9764486f711b7908b4ff9594f55057667
Mohamad Vajid <42029091+mvajid@users.noreply.github.com>
2020-06-08 15:41:59 +0400

    Add files via upload

---
+++ Helpers.ps1
@@ -0,0 +1,24 @@
+﻿#Function to Create-AesKey
+function Create-AesKey {
+   $aesManaged = New-Object "System.Security.Cryptography.AesManaged"
+   $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::CBC
+   $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
+   $aesManaged.BlockSize = 128
+   $aesManaged.KeySize = 256
+   $aesManaged.GenerateKey()
+   [System.Convert]::ToBase64String($aesManaged.Key)
+}
+
+#Function to Display Output Values 
+function DisplayValues {
+
+Write-Host (" ")
+Write-Host ("Client ID: " + $myApp.AppId)
+Write-Host ("Client Secret: " + $AppKeyValue)
+Write-Host (" ")
+Write-Host ("Callback URL:") $appURL
+Write-Host ("Auth URL:") "https://login.windows.net/$tenantid/oauth2/authorize?resource=https://api.businesscentral.dynamics.com"
+Write-Host ("Access Token URL:") "https://login.windows.net/$tenantid/oauth2/token?resource=https://api.businesscentral.dynamics.com"
+Write-Host (" ")
+
+}
---
+++ OAuth2TokenRequest.ps1
@@ -0,0 +1,37 @@
+﻿######################################################
+#   Script to create an Azure App for OAuth2 token   #
+#                    [mvajid]                        #
+######################################################
+
+Import-Module .\Helpers.ps1
+
+#Set Tenant Variables
+$tenantid = Read-Host -Prompt "Enter Tenant ID"
+$environmentname = Read-Host -Prompt "Enter Environment Name"
+
+#Install AzureAD
+#Install-Module AzureAD
+
+#Connect to AzureAD Tenant
+Connect-AzureAD -TenantId $tenantid
+
+#Set Variables for App
+$appName = "OAuth2 Token for API Access"
+$appURL = "https://businesscentral.dynamics.com/$tenantid/$environmentname/"
+
+#Set required Resource Access for Dynamcis 365 Business Central
+$req = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess" 
+$req.ResourceAppId = "996def3d-b36c-4153-8607-a6fd3c01b89f"
+$req.ResourceAccess = New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList "bce0976a-cb0b-473b-8800-84eda9f8e447","Scope"
+
+#Create App
+$myApp = New-AzureADApplication -DisplayName $appName -ReplyUrls $appURL -RequiredResourceAccess $req
+
+#Create Client secret for App
+$startDate = Get-Date
+$AppKeyValue = Create-AesKey
+New-AzureADApplicationPasswordCredential -ObjectId $myApp.ObjectId -Value $AppKeyValue -CustomKeyIdentifier "OAuth2" -StartDate $startDate -EndDate $startDate.AddYears(10)
+
+#Moment of truth
+clear
+DisplayValues
